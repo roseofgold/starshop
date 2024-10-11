59e60048c455c73ddee902d6f7fcad47abe13b78
Melinda Serven <melinda.serven@acquia.com>
2024-10-11 12:10:25 -0700

    Generating URLs

---
+++ src/Controller/StarshipController.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\StarshipRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipController extends AbstractController
+{
+    #[Route('/starships/{id<\d+>}', name: 'app_starship_show')]
+    public function show(int $id, StarshipRepository $starshipRepository):Response
+    {
+        $ship = $starshipRepository->find($id);
+
+        if (!$ship) {
+            throw $this->createNotFoundException('Starship not found.');
+        }
+
+        return $this->render('starship/show.html.twig', [
+            'ship' => $ship,
+        ]);
+    }
+
+}
