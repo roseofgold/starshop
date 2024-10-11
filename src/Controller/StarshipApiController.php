3cc582a94cbdb7b40fd5336035c0d73e2fe2a464
Melinda Serven <melinda.serven@acquia.com>
2024-10-11 10:56:56 -0700

    Fancier Routes

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -9,13 +9,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships')]
+    #[Route('', name: 'api_starships', methods: ['GET'])]
     public function getCollection(StarshipRepository $starshipRepository): Response
     {
         $starships = $starshipRepository->findAll();
 
         return $this->json($starships);
+    }
+
+    #[Route('/{id<\d+>}', name: 'api_starship', methods: ['GET'])]
+    public function get(int $id, StarshipRepository $starshipRepository): Response
+    {
+        $starship = $starshipRepository->find($id);
+
+        if (!$starship) {
+            throw $this->createNotFoundException('Starship not found.');
+        }
+
+        return $this->json($starship);
     }
 }
